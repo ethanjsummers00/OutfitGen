f5381a194fd777c4bc6477e7d14d936a02117ea5
Ethan <ethanjsummers00@gmail.com>
2022-02-14 17:11:04 -0700

    Added Placeholders and Script Outline
    - Added placeholder tops to test loading images
    - added script to load from array

--- sketch.js
+++ sketch.js
@@ -1,6 +1,27 @@
-let topVar
-let bottomVar
-let shoesVar
+let topVar;
+let bottomVar;
+let shoesVar;
+
+let top;
+let bottom;
+let shoes;
+
+//let topChoices = []
+//let bottomChoices = []
+//let shoesChoices = []
+
+function preload() {
+  topVar = [
+    "/images/faketop1.png",
+    "/images/faketop2.png",
+    "/images/faketop3.png"
+  ];
+
+  let topPick = floor(random(topVar.length));
+
+  top = loadImage(topVar[pos]);
+}
+
 
 function setup() {
   createCanvas(900, 900);
@@ -8,4 +29,6 @@ function setup() {
 
 function draw() {
   background(220);
+
+  image(top, 0, 0);
 }
